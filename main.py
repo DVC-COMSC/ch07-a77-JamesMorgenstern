2165b4f53e53b668ca085e48ba74d2776d063ad3
JamesMorgenstern <jamesklm@comcast.net>
2022-10-09 14:16:57 -0700

    committed2

--- main.py
+++ main.py
@@ -3,13 +3,23 @@ num1 = list(map(int, input().split()))
 num2 = list(map(int, input().split()))
 num3 = []
 
-for i in range(len(num1)):
-    num3.append(num1[i])
-    num3.append(num2[i])
-    j = i
+if (len(num1) <= len(num2)):
+    for i in range(len(num1)):
+        num3.append(num1[i])
+        num3.append(num2[i])
+        j = i
 
-while (j < len(num2) - 1):
-    j += 1
-    num3.append(num2[j])
+    while (j < len(num2) - 1):
+        j += 1
+        num3.append(num2[j])
+else:
+    for i in range(len(num2)):
+        num3.append(num1[i])
+        num3.append(num2[i])
+        j = i
+
+    while (j < len(num1) - 1):
+        j += 1
+        num3.append(num1[j])
 
 print (num3) 
